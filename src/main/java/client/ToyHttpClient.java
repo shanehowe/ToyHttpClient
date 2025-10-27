2e6436a58466feb1ebc2217437e0e1826999a4ac
Shane Howe <shanehowe100@gmail.com>
2025-10-27 12:29:56 +0000

    Refactor DynamicHttpTransport to use injected transport instances

--- src/main/java/client/ToyHttpClient.java
+++ src/main/java/client/ToyHttpClient.java
@@ -4,12 +4,16 @@ import java.io.IOException;
 import messaging.ToyHttpRequest;
 import messaging.ToyHttpResponse;
 import transport.DynamicHttpTransport;
+import transport.PlainHttpTransport;
+import transport.SecureHttpTransport;
 import transport.ToyHttpTransport;
 
 public record ToyHttpClient(ToyHttpTransport transport) {
 
   public static ToyHttpClient newDefaultClient() {
-    return new ToyHttpClient(new DynamicHttpTransport());
+    PlainHttpTransport plainHttpTransport = new PlainHttpTransport();
+    SecureHttpTransport secureHttpTransport = new SecureHttpTransport();
+    return new ToyHttpClient(new DynamicHttpTransport(secureHttpTransport, plainHttpTransport));
   }
 
   public ToyHttpResponse send(ToyHttpRequest request) throws IOException {
--- src/main/java/transport/DynamicHttpTransport.java
+++ src/main/java/transport/DynamicHttpTransport.java
@@ -6,15 +6,21 @@ import messaging.ToyHttpResponse;
 
 public class DynamicHttpTransport implements ToyHttpTransport {
 
+  private final ToyHttpTransport secureTransport;
+  private final ToyHttpTransport plainTransport;
+
+  public DynamicHttpTransport(ToyHttpTransport secureTransport, ToyHttpTransport plainTransport) {
+    this.secureTransport = secureTransport;
+    this.plainTransport = plainTransport;
+  }
+
   @Override
   public ToyHttpResponse send(ToyHttpRequest request) throws IOException {
-    ToyHttpTransport delegate =
-        switch (request.uri().getScheme().toLowerCase()) {
-          case "https" -> new SecureHttpTransport();
-          case "http" -> new PlainHttpTransport();
-          default ->
-              throw new IllegalArgumentException("can only send http request over http or https");
-        };
-    return delegate.send(request);
+    return switch (request.uri().getScheme().toLowerCase()) {
+      case "https" -> secureTransport.send(request);
+      case "http" -> plainTransport.send(request);
+      default ->
+          throw new IllegalArgumentException("can only send http request over http or https");
+    };
   }
 }
